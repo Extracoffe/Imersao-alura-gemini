39a348c789d6e59d8faea59dcd079efced7f6c42
André <andrerodartematos@gmail.com>
2024-09-04 18:17:24 -0300

    aula 3 terminada

---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = [
+    {
+        titulo : "Braincore",
+        descricao : "Site feito para os cursos, metodologia, professores e contato da Braincore",
+        link : "https://braincore.com.br/"
+    },
+    {
+        titulo : "Receitas Saudáveis",
+        descricao : "App feito para você fazer receitas saudáveis.",
+        link : "https://play.google.com/store/apps/details?id=com.exemplo.receitas"
+    },
+    {
+        titulo : "Loja de roupas Auto Sustentáveis",
+        descricao : "Site para comprar roupas sustentáveis.",
+        link : "https://www.lojavirtual.com.br"
+    }
+];
+
