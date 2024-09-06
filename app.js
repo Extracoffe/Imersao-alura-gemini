ba3da026b17f1bdfd0a131198cfbecda8d3b74db
Extracoffe <andrerodartematos@gmail.com>
2024-09-06 10:21:12 -0300

    Imersão alura finalizada.

--- app.js
+++ app.js
@@ -1,24 +1,50 @@
-// Função para fazer o botão de pesquisa ficar funcional.
-function pesquisar(){
-    let section = document.getElementById("resultados-pesquisa")
-//console.log(section);
+    // Função para fazer o botão de pesquisa ficar funcional.
+    function pesquisar(){
+        let section = document.getElementById("resultados-pesquisa");
 
-let resultados = ""
+    let campoPesquisa = document.getElementById("campo-pesquisa").value 
 
-//para cada dado dentro da lista de dados
-for (let dado of dados) {
-    resultados += `
-    <div class="item-resultado">
-        <h2>${dado.titulo}</h2>
-        <p class="descricao-meta">
-            ${dado.descricao}
-        </p>
-        <p><a href=${dado.link} target="_blank">Mais informações</a></p>
-    </div>
-`
-}
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado. Você não buscou por nada.<p>"
+        return 
+    };
 
-section.innerHTML = resultados
-}
+    campoPesquisa = campoPesquisa.toLowerCase()
 
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
 
+    //para cada dado dentro da lista de dados
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        // se titulo includes campoPesquisa
+        if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // Cria um novo elemento
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                    ${dado.titulo}
+                </h2>
+                <p class="descricao-meta">
+                    ${dado.descricao}
+                </p>
+                <p>
+                    <a href=${dado.link} target="_blank">
+                    Mais informações
+                    </a>
+                </p>
+            </div>
+            `;
+        }       
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado.</p>"
+    }
+
+    section.innerHTML = resultados
+    }
--- dados.js
+++ dados.js
@@ -2,17 +2,20 @@ let dados = [
     {
         titulo : "Braincore: Educação e Tecnologia",
         descricao : "Site feito para os cursos, metodologia, professores e contato da Braincore",
-        link : "https://braincore.com.br/"
+        link : "https://braincore.com.br/",
+        tags: "site educação crianças idosos reforço digitalmente python"
     },
     {
         titulo : "Receitas Saudáveis",
         descricao : "App feito para você fazer receitas saudáveis.",
-        link : "https://play.google.com/store/apps/details?id=com.exemplo.receitas"
+        link : "https://play.google.com/store/apps/details?id=com.exemplo.receitas",
+        tags: ""
     },
     {
         titulo : "Loja de roupas Auto Sustentáveis",
         descricao : "Site para comprar roupas sustentáveis.",
-        link : "https://www.lojavirtual.com.br"
+        link : "https://www.lojavirtual.com.br",
+        tags: ""
     }
 ];
 
